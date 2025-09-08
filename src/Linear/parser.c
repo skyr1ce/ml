16837c30aa9158f6c9a49bc54f255c0f47064637
skyr1ce <142119948+skyr1ce@users.noreply.github.com>
2025-09-08 07:30:37 -0400

    Update parser.c

--- src/Linear/parser.c
+++ src/Linear/parser.c
@@ -1,14 +1,31 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<stdbool.h>
+#include"parser.h"
 
 double* parseX(bool x, long int maxpt) {
-  double *x = malloc (sizeof(double) * maxpt);
-  FILE *file; 
-  if (x) {
-    file = fopen ("data/x.txt", "r"); 
-  } else {
-    file = fopen("data/y.txt", "r"); 
-  }
+        FILE *file; 
+        if (x) {
+                file = fopen("./data/x.txt", "r"); 
+        } else {
+                file = fopen("./data/y.txt", "r"); 
+        }
+        double *X = malloc(sizeof(double) * (size_t)maxpt); 
+        if (file == NULL) {
+                perror("Error opening file"); 
+                exit(1); 
+        }
+        long r = 0; 
+        double v;
+        while (r < maxpt && fscanf(file, " %lf", &v) == 1) {
+                X[(size_t)r] = v; 
+                r++; 
+        }
+        fclose(file);
+        for (long i = 0; i < r; i++) {
+                printf("%lf ", X[i]); 
+        }
 
+        printf("\n"); 
+        return X; 
 }
